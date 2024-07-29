b69afb1006e2c8dfe2581b5c7d42cc395c4fa242
Lucas Vas <lvas463@gmail.com>
2024-07-29 02:29:52 -0400

    fix: updated pre-/suffix to display more reasonably

--- src/customElements/OutlinedTextField.ts
+++ src/customElements/OutlinedTextField.ts
@@ -1,7 +1,6 @@
 import { LitElement, html, css, CSSResultGroup, HTMLTemplateResult } from "lit";
 import { customElement, property, query } from "lit/decorators.js";
 
-
 @customElement("outlined-text-field")
 export class OutlinedTextField extends LitElement {
   // Input height was 44px
@@ -16,7 +15,8 @@ export class OutlinedTextField extends LitElement {
       margin-top: 6px;
       height: 48px;
       position: relative;
-      transition: border-color 400ms ease,
+      transition:
+        border-color 400ms ease,
         background-color 400ms ease;
       &:has(input:disabled) {
         border-color: black;
@@ -30,8 +30,6 @@ export class OutlinedTextField extends LitElement {
       border: none;
       padding: 2px 15px;
       color: #696158;
-      padding-right: 15px;
-      padding-left: 15px;
       border-radius: 8px;
       &:focus {
         outline: none;
@@ -53,7 +51,8 @@ export class OutlinedTextField extends LitElement {
       justify-content: center;
     }
 
-    input:focus ~ label, input:not(:placeholder-shown) ~ label {
+    input:focus ~ label,
+    input:not(:placeholder-shown) ~ label {
       top: -7px;
       font-size: 11pt;
       font-color: gray;
@@ -64,23 +63,47 @@ export class OutlinedTextField extends LitElement {
       box-shadow: none;
     }
 
-    span .prefix {
+    .prefix {
       height: auto;
       width: auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      pointer-events: none;
+      margin-left: 5px;
+    }
 
+    .suffix {
+      height: auto;
+      width: auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      pointer-events: none;
+      margin-right: 5px;
     }
   `;
 
-  @property({type: String}) accessor placeholder: string = "";
-  @property({type: String}) accessor width: string = "300px";
-  @property({type: String, attribute: "suffix-text"}) accessor suffixText: string = "";
-  @property({type: String, attribute: "prefix-text"}) accessor prefixText: string = "";
-  @property({type: String}) accessor pattern: string = "";
-  @property({type: Boolean, reflect: true}) accessor disabled: boolean = false;
-  @property({type: Boolean, reflect: true}) accessor required: boolean = false;
-  @property({type: String}) accessor type: string = "text";
-  @property({type: String}) accessor value: string = "";
-  @property({type: String}) accessor name: string = "";
+  @property({ type: String })
+  accessor placeholder: string = "";
+  @property({ type: String })
+  accessor width: string = "300px";
+  @property({ type: String, attribute: "suffix-text" })
+  accessor suffixText: string = "";
+  @property({ type: String, attribute: "prefix-text" })
+  accessor prefixText: string = "";
+  @property({ type: String })
+  accessor pattern: string = "";
+  @property({ type: Boolean, reflect: true })
+  accessor disabled: boolean = false;
+  @property({ type: Boolean, reflect: true })
+  accessor required: boolean = false;
+  @property({ type: String })
+  accessor type: string = "text";
+  @property({ type: String })
+  accessor value: string = "";
+  @property({ type: String })
+  accessor name: string = "";
 
   @query("input") private accessor _input: HTMLInputElement;
 
@@ -97,23 +120,26 @@ export class OutlinedTextField extends LitElement {
       <div class="container">
         ${this.renderPrefix()}
         <!-- There is a space character as a placeholder, which is slightly hacky, but works with the css :placeholder-shown -->
-        <input type=${this.type} placeholder=" "
+        <input
+          type=${this.type}
+          placeholder=" "
           ?disabled=${this.disabled}
           ?required=${this.required}
           name=${this.name}
           pattern=${this.pattern}
-          @change=${this._handleChange}/>
+          @change=${this._handleChange}
+        />
         ${this.renderSuffix()}
         <label>${this.placeholder}</label>
       </div>
-    `; 
+    `;
   }
 
   private renderPrefix(): HTMLTemplateResult {
-    return html`<span class="prefix">${this.prefixText}</span>`;
+    return html`<span class="prefix"><p>${this.prefixText}</p></span>`;
   }
 
   private renderSuffix(): HTMLTemplateResult {
-    return html`<span class="suffix">${this.suffixText}</span>`;
+    return html`<span class="suffix"><p>${this.suffixText}</p></span>`;
   }
 }
