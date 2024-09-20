cbc118f6680cb5c9a405fbd766a97a2cbc2f904d
Lucas Vas <lvas463@gmail.com>
2024-09-20 09:35:02 -0400

    Commiting to get changes from main

--- src/customElements/MultipleEntry.ts
+++ src/customElements/MultipleEntry.ts
@@ -1,12 +1,25 @@
-import { LitElement, html, css, CSSResultGroup, HTMLTemplateResult } from 'lit-element';
-import { customElement, property, query } from 'lit-element/decorators.js';
+import {
+  LitElement,
+  html,
+  css,
+  CSSResultGroup,
+  HTMLTemplateResult,
+} from "lit-element";
+import { customElement, property, query } from "lit-element/decorators.js";
 
-@customElement('multi-entry')
+type FormEntry = {
+  baseElement: CustomEntry;
+  // This must be an any, because we can't predict the object that will be passed.
+  entryData: any;
+};
+
+@customElement("multi-entry")
 export class MultiEntry extends LitElement {
   static styles?: CSSResultGroup = css``;
 
-  @property({type: Boolean, reflect: true}) accessor required: boolean = false;
-  @property({type: {}}) private accessor _entries: Array<any> = [];
+  @property({ type: Boolean, reflect: true })
+  accessor required: boolean = false;
+  @property({ type: {} }) private accessor _entries: Array<FormEntry> = [];
   @query('slot[name="entries"]') private accessor _entriesSlot: HTMLSlotElement;
 
   protected render(): HTMLTemplateResult {
@@ -21,17 +34,58 @@ export class MultiEntry extends LitElement {
   }
 
   addEntry(): void {
-    // Create an entry element and append it to the entries array.
-    const entry = document.createElement('div');
-    entry.slot = 'entries';
-    this._entries.push(entry);
+    // TODO This will need to open a modal window in order
+    // to get the information required for the new element.
+
+    // Create an entry HTMLElement
+    const entry = document.createElement("entry");
+
+    // Make a new FormEntry object and store in array.
+    // TODO Make this object not a testing object.
+    const entryObject: FormEntry = {
+      baseElement: entry as CustomEntry,
+      entryData: {
+        thing: "hello",
+        otherThing: "hello again",
+        thingCount: this._entries.length,
+      },
+    };
+    this._entries.push(entryObject);
 
     // Give the new element some content.
-    const entryContent = document.createElement('p');
-    entryContent.textContent = 'Testing info!';
+    const entryContent = document.createElement("p");
+    entryContent.textContent = "Testing info!";
     entry.appendChild(entryContent);
 
-    // Render the element.
+    // Add the element to the DOM.
     this._entriesSlot.appendChild(entry);
+  }
+}
+
+@customElement("custom-entry")
+export class CustomEntry extends LitElement {
+  // This element will handle the entry elements within the multi-entry element.
+  // I'm not sure if this is needed just yet.
+
+  static styles: CSSResultGroup = css`
+    div .container {
+      display: flex;
+      flex-direction: row;
+    }
+
+    slot {
+      display: flex;
+      flex-direction: row;
+    }
+  `;
+
+  protected render(): HTMLTemplateResult {
+    return html`
+      <div class="container">
+        <slot name="info">
+          <!-- Any display information about this entry goes here. -->
+        </slot>
+      </div>
+    `;
   }
 }
