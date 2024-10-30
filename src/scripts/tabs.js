27f8932e435767ea9b300b6ba4355f91c74d501e
Lucas Vas <lvas463@gmail.com>
2024-10-30 12:29:22 -0400

    created tab-specific script

---
+++ src/scripts/tabs.js
@@ -0,0 +1,13 @@
+// This script simply adds functionality to tab elements.
+$(function() {
+  const activeTab = document.querySelector('tab-bar').activeTab;
+  $(`#${activeTab.panelId}`).attr('active', true);
+
+  $('tab-bar').on('change', () => {
+    const tabBar = document.querySelector('tab-bar');
+    const panelId = tabBar.activeTab.panelId;
+
+    $('tab-panel').removeAttr('active');
+    $(`#${panelId}`).attr('active', true);
+  });
+})
