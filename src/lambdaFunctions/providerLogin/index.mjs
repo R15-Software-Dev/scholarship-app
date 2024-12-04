0b24de1b8456aacb76b7138aa2cf521d74e58e89
Lucas Vas <lvas463@gmail.com>
2024-12-04 08:59:36 -0500

    refactor: add the jose package

--- src/lambdaFunctions/providerLogin/index.mjs
+++ src/lambdaFunctions/providerLogin/index.mjs
@@ -1,4 +1,4 @@
-import { sign } from "jsonwebtoken";
+import * as jose from "jose";
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 
 
@@ -35,14 +35,25 @@ export const handler = async (event) => {
       return "Password does not match";
     }
 
+    // If password matches, get the secret from Secrets Manager
+    const secretResponse = await secretClient.send(new GetSecretValueCommand({
+      SecretId: "providerjwt"
+    }));
+    const secret = secretResponse.SecretString;
+
     // Create JWT and return it
-    // TODO Put real values in the iss and aud fields
-    const token = sign({
-      iss: "api/url/path",
+    const token = await jose.jwtSign({
+      iss: "https://smwldja6ql.execute-api.us-east-1.amazonaws.com/sclshp-form/login",
+      aud: "https://alphafetus-testbucket.s3.amazonaws.com/entryPortal.html",
       sub: dbresponse.Item.UserID.S,
-      name: dbresponse.Item.Username,
-      aud: "login/website/path",
-    });
+      user: dbresponse.Item.Username.S,
+      email: dbresponse.Item.Email.S
+    }).setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setIssuer("https://smwldja6ql.execute-api.us-east-1.amazonaws.com/sclshp-form/login")
+    .setAudience("https://alphafetus-testbucket.s3.amazonaws.com/entryPortal.html")
+    .setExpirationTime("1w")
+    .sign(secret);
 
     // Return the JWT - this should be stored in the client's local storage
     return token;
