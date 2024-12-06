f615b8410600cb9d5f15500bef2c9e3d72f635db
Lucas Vas <lvas463@gmail.com>
2024-12-06 13:01:20 -0500

    fix: swapped files
    Accidentally put code in the wrong file.

--- src/lambdaFunctions/providerAuthorizer/index.mjs
+++ src/lambdaFunctions/providerAuthorizer/index.mjs
@@ -1,80 +1,32 @@
 import * as jose from "jose";
-import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 
 
-// Create a DynamoDB client
-const client = new DynamoDBClient({ region: "us-east-1" });
-// Create a Secrets client
 const secretClient = new SecretsManagerClient({ region: "us-east-1" });
 
 /**
- * Checks login information and creates a JWT for authorization
- *
- * @param event - Event body from API request
- * @returns A JWT with the user's information or an authorization error
- * @type {(event: AuthProvider) => string | Error}
- */
+* Checks if a token has the correct signature and is not expired
+* @param event - Event body from API request. Should include a token field.
+* @returns A boolean indicating if the token is valid
+* @type {(event: { token: string }) => boolean}
+*/
 export const handler = async (event) => {
   console.log("Recieved event: ");
   console.log(event);
-  // Create command to get user information
-  const getCommand = new GetItemCommand({
-    TableName: "scholarship-providers",
-    Key: {
-      Email: { S: event.email },
-    },
-    AttributesToGet: [
-      "Password",
-      "Email",
-      "UserID"
-    ]
+  // Get the secret from Secrets Manager
+  const secretResponse = await secretClient.send(new GetSecretValueCommand({
+    SecretId: "providerjwt"
+  })
+  );
+  const secret = secretResponse.SecretString;
+
+  // Verify the token
+  // TODO Put real values in the iss and aud fields
+  let { payload, protectedHeader } = jose.jwtVerify(event.token, secret, {
+    iss: "https://smwldja6ql.execute-api.us-east-1.amazonaws.com/login",
+    aud: "https://alphafetus-testbucket.s3.amazonaws.com/entryPortal.html"
   });
 
-  // Send command to DynamoDB and check if the password matches
-  const dbresponse = await client.send(getCommand);
-  console.log("Database response: ");
-  console.log(dbresponse);
-  if (dbresponse.Item.Password.S !== event.password ||
-    dbresponse.Item.Email.S !== event.email) {
-    throw Error("Password does not match");
-  }
-
-  // If password matches, get the secret from Secrets Manager
-  const secretResponse = await secretClient.send(new GetSecretValueCommand({
-    "SecretId": "providerjwt"
-  }));
-  const secret = new TextEncoder().encode(secretResponse.SecretString);
-
-  // Create JWT and return it
-  const token = await new jose.SignJWT({
-    sub: dbresponse.Item.UserID.N,
-    email: dbresponse.Item.Email.S
-  })
-  .setIssuedAt()
-  .setIssuer("https://smwldja6ql.execute-api.us-east-1.amazonaws.com/sclshp-form/login")
-  .setAudience("https://alphafetus-testbucket.s3.amazonaws.com/entryPortal.html")
-  .setExpirationTime("1w")
-  .setProtectedHeader({ alg: "HS256" })
-  .sign(secret);
-
-  // Return the JWT - this should be stored in the client's local storage
-  return token;
-}
-
-/**
-* Template class for the authentication request body.
-*/
-class AuthProvider {
-  /**
-  * Email address of the user
-  * @type {string}
-  */
-  email = "";
-
-  /**
-  * Password of the user. This should already be hashed.
-  * @type {string}
-  */
-  password = "";
+  console.log(payload);
+  return true;
 }
--- src/lambdaFunctions/providerLogin/index.mjs
+++ src/lambdaFunctions/providerLogin/index.mjs
@@ -1,9 +1,12 @@
 import * as jose from "jose";
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 
 
 // Create a DynamoDB client
 const client = new DynamoDBClient({ region: "us-east-1" });
+// Create a Secrets client
+const secretClient = new SecretsManagerClient({ region: "us-east-1" });
 
 /**
  * Checks login information and creates a JWT for authorization
@@ -13,56 +16,50 @@ const client = new DynamoDBClient({ region: "us-east-1" });
  * @type {(event: AuthProvider) => string | Error}
  */
 export const handler = async (event) => {
-  try {
-    console.log("Recieved event: ");
-    console.log(event);
-    // Create command to get user information
-    const getCommand = new GetItemCommand({
-      TableName: "scholarship-providers",
-      Key: {
-        email: { S: event.email }
-      },
-      AttributesToGet: [
-        "Password",
-        "Username",
-        "Email",
-        "UserID"
-      ]
-    });
+  console.log("Recieved event: ");
+  console.log(event);
+  // Create command to get user information
+  const getCommand = new GetItemCommand({
+    TableName: "scholarship-providers",
+    Key: {
+      Email: { S: event.email },
+    },
+    AttributesToGet: [
+      "Password",
+      "Email",
+      "UserID"
+    ]
+  });
 
-    // Send command to DynamoDB and check if the password matches
-    const dbresponse = await client.send(getCommand);
-    if (dbresponse.Item.Password.S !== event.password ||
-      dbresponse.Item.Email.S !== event.email) {
-      return "Password does not match";
-    }
-
-    // If password matches, get the secret from Secrets Manager
-    const secretResponse = await secretClient.send(new GetSecretValueCommand({
-      SecretId: "providerjwt"
-    }));
-    const secret = secretResponse.SecretString;
-
-    // Create JWT and return it
-    const token = await jose.jwtSign({
-      iss: "https://smwldja6ql.execute-api.us-east-1.amazonaws.com/sclshp-form/login",
-      aud: "https://alphafetus-testbucket.s3.amazonaws.com/entryPortal.html",
-      sub: dbresponse.Item.UserID.S,
-      user: dbresponse.Item.Username.S,
-      email: dbresponse.Item.Email.S
-    }).setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setIssuer("https://smwldja6ql.execute-api.us-east-1.amazonaws.com/sclshp-form/login")
-    .setAudience("https://alphafetus-testbucket.s3.amazonaws.com/entryPortal.html")
-    .setExpirationTime("1w")
-    .sign(secret);
-
-    // Return the JWT - this should be stored in the client's local storage
-    return token;
-  } catch (error) {
-    console.error(error);
-    throw new Error(error.message);
+  // Send command to DynamoDB and check if the password matches
+  const dbresponse = await client.send(getCommand);
+  console.log("Database response: ");
+  console.log(dbresponse);
+  if (dbresponse.Item.Password.S !== event.password ||
+    dbresponse.Item.Email.S !== event.email) {
+    throw Error("Password does not match");
   }
+
+  // If password matches, get the secret from Secrets Manager
+  const secretResponse = await secretClient.send(new GetSecretValueCommand({
+    "SecretId": "providerjwt"
+  }));
+  const secret = new TextEncoder().encode(secretResponse.SecretString);
+
+  // Create JWT and return it
+  const token = await new jose.SignJWT({
+    sub: dbresponse.Item.UserID.N,
+    email: dbresponse.Item.Email.S
+  })
+  .setIssuedAt()
+  .setIssuer("https://smwldja6ql.execute-api.us-east-1.amazonaws.com/sclshp-form/login")
+  .setAudience("https://alphafetus-testbucket.s3.amazonaws.com/entryPortal.html")
+  .setExpirationTime("1w")
+  .setProtectedHeader({ alg: "HS256" })
+  .sign(secret);
+
+  // Return the JWT - this should be stored in the client's local storage
+  return token;
 }
 
 /**
