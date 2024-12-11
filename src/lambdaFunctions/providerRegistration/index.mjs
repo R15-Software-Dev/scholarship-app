742cd36eca2b4b2757b540f84919c80224bcc460
Lucas Vas <lvas463@gmail.com>
2024-12-11 08:26:36 -0500

    feat: add registration database commands

---
+++ src/lambdaFunctions/providerRegistration/index.mjs
@@ -0,0 +1,63 @@
+import { DynamoDBClient, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
+
+// Create a DynamoDB client
+const client = new DynamoDBClient({ region: "us-east-1" });
+
+/**
+ * Registers a provider in the database
+ *
+ * @param event - Event body from API request
+ * @returns A success message or an error
+ * @type {(event: Provider) => string | Error}
+ */
+
+export const handler = async (event) => {
+  // Create a command to check if the provider is already registered.
+  // If the provider is already registered, return an error and do not register them again.
+  const getCommand = new GetItemCommand({
+    TableName: "scholarship-providers",
+    Key: {
+      Email: { S: event.email }
+    },
+    AttributesToGet: [
+      "Email"
+    ]
+  });
+
+  // Send command to DynamoDB
+  let dbresponse = await client.send(getCommand);
+
+  // If the provider is already registered, return an error
+  if (!dbresponse.Item) {
+    // Create a command to add the provider to the database
+    const putCommand = new PutItemCommand({
+      TableName: "scholarship-providers",
+      Item: {
+        Email: { S: event.email },
+        Password: { S: event.password }
+      }
+    });
+
+    // Send command to DynamoDB
+    dbresponse = await client.send(putCommand);
+
+    return "Provider registered successfully";
+  }
+
+  return "Provider already registered";
+}
+
+class Provider {
+  /**
+   * Email address of the provider.
+   * @type {string}
+   */
+  email = "";
+
+  /**
+   * Password of the provider.
+   * This value should be hashed before being sent here.
+   * @type {string}
+   */
+  password = "";
+}
