014955a32dbc1546b85918ade1f68a3c599380e8
Lucas Vas <lvas463@gmail.com>
2024-12-13 13:27:53 -0500

    build: change from webpack to esbuild

---
+++ src/customElements/customElements.ts
@@ -0,0 +1,12 @@
+import "lit";
+
+// import "./ActionButton";
+// import "./Checkbox";
+import "./Dropdown";
+import "./Forms";
+import "./InputElement";
+import "./modalWindow";
+import "./MultipleEntry";
+import "./Ripple";
+import "./Tabs";
+import "./TextField";
