c862f67245b86c4203b2fc4794283f2b9be1adcf
Lucas Vas <lvas463@gmail.com>
2024-12-25 21:53:44 -0500

    fix: remove buggy code and rearrange

--- src/scripts/entryPortal.ts
+++ src/scripts/entryPortal.ts
@@ -1,10 +1,20 @@
-import {OutlinedTextField} from "../customElements/TextField";
 import {Scholarship} from "../lambdaFunctions/types/scholarship";
-import {Checkbox} from "../customElements/Checkbox";
-import {Dropdown} from "../customElements/Dropdown";
 import {InputElement} from "../customElements/InputElement";
 
 $(async function () {
+  // Handles showing the essay selections when the option to pick them is shown.
+  document.querySelector("#essayRequirementOption").addEventListener("change", function () {
+    const essaySelection = $("#essayRequirementSelection");
+    if (this.selectedCheckbox[0] === "Yes") {
+      // Show the box
+      essaySelection.css("display","block");
+    } else {
+      // Don't show a box
+      essaySelection.css("display","none");
+    }
+  });
+
+  //#region Form Initialization
   const apiBase = "http://localhost:3000";
   // Initialize the forms
   // First get the information from the database
@@ -16,43 +26,12 @@ $(async function () {
   Object.entries(scholarship).forEach(([key, value]) => {
     // Update the corresponding input element with the value
     let input: InputElement = document.querySelector(`#${key}Input`);
-    input.setValue(value);
   });
-  
-  
-  // Listen for change event on email input
-  $("#sclshpConfirmEmailInput").on("focusout", function () {
-    console.log("Checking emails");
-    const emailInput = document.querySelector(
-      "#sclshpContactEmailInput",
-    )._input;
-    const compEmailInput = document.querySelector(
-      "#sclshpConfirmEmailInput",
-    )._input;
-
-    if (emailInput.value !== compEmailInput.value) {
-      console.log("Emails do not match, showing error.");
-      compEmailInput.showErrorString("Emails do not match.");
-    } else {
-      // Clear errors.
-      compEmailInput.hideErrors();
-    }
-  });
+  //#endregion
 
   // Function scrolls to the top of the page when a tab is clicked
   $('tab-bar').on('click', function () {
     // Scroll to the top of the page smoothly
     $('html, body').animate({ scrollTop: 0 }, 'smooth');
   });
-
-  $("#essayRequirementOption").on("change", function () {
-    const essaySelection = $("#essayRequirementSelection");
-    if (this.selectedCheckbox[0] === "Yes") {
-      // Show the box
-      essaySelection.css("display","block");
-    } else {
-      // Don't show a box
-      essaySelection.css("display","none");
-    }
-  })
 });
