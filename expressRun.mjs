cfca3994db9e02df4521f2cf92796947f2ebcb57
Lucas Vas <lvas463@gmail.com>
2024-12-28 21:24:58 -0500

    build: add expressjs and a proxy

---
+++ expressRun.mjs
@@ -0,0 +1,20 @@
+﻿import express from 'express';
+import proxy from 'express-http-proxy';
+
+const app = express();
+
+// Proxy specific API requests
+app.use("/providers", proxy("http://localhost:3000", {
+  proxyReqPathResolver: function (req) {
+    console.log(req.originalUrl);
+    return req.originalUrl;
+  }
+}));
+
+// Serve static files from the dist directory
+app.use(express.static('dist'));
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+  console.log("Connect by using http://localhost:8080");
+});
