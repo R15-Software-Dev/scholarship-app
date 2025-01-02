3ac171715e253e2f041e23dd3bc258167c3ad2b1
Lucas Vas <lvas463@gmail.com>
2025-01-01 21:41:12 -0500

    fix: properly show error messages on login

--- src/scripts/providerLogin.ts
+++ src/scripts/providerLogin.ts
@@ -4,62 +4,78 @@ $(function() {
 
   // We will need to get the values for the form submission manually
   // Set up a listener on the login form
-  $("#login-form").on("submit", async (event) => {
+  document.querySelector("#login-form").addEventListener("submit", async event => {
+  // $("#login-form").on("submit", async (event) => {
     event.preventDefault();
 
     // Get the values from the form elements.
+    const form = event.target as HTMLFormElement;
+    const allInputs = form.querySelectorAll("form-question") as NodeListOf<FormQuestion>;
     const emailInput = this.querySelector("#loginEmailInput") as FormQuestion;
     const passwordInput = this.querySelector("#loginPasswordInput") as FormQuestion;
 
-    const values = {
-      email: emailInput.input.getValue(),
-      password: passwordInput.input.getValue(),
-    };
+    allInputs.forEach(question => question.input.clearError());
 
-    // Only for debugging.
-    // console.log(values);
-
-    // Send these values to the API and wait for a response.
-    // We'll react to the response's statusCode accordingly.
-    // The API url will need to match our API's url and be tested through AWS as well.
-    const pendingButton = $('#loginButton');
-    const loginErrorDiv = $('#divErrorLogin');
-
-    try {
-      pendingButton.addClass("disabled");
-      loginErrorDiv.removeClass('shown');
-      const response = await fetch("/providers/login", {
-        method: "POST",
-        body: JSON.stringify(values)
-      });
-
-      if (!response.ok) {
-        // This means there's something wrong with the api.
-        // TODO Show some error.
+    // Check validity of inputs
+    let submittable = true;
+    allInputs.forEach((question) => {
+      if (!question.checkValidity()) {
+        submittable = false;
+        question.input.displayError("Invalid input");
       }
+    })
 
-      const responseJson = await response.json();
+    if (submittable) {
+      const values = {
+        email: emailInput.input.getValue(),
+        password: passwordInput.input.getValue(),
+      };
 
-      if (responseJson.message === "Login successful") {
-        // Redirect to the entryPortal page.
-        window.location.replace("./entryPortal.html");
-      } else {
-        loginErrorDiv.html(responseJson.message);
-        loginErrorDiv.addClass("shown");
+      // Only for debugging.
+      // console.log(values);
+
+      // Send these values to the API and wait for a response.
+      // We'll react to the response's statusCode accordingly.
+      // The API url will need to match our API's url and be tested through AWS as well.
+      const pendingButton = $('#loginButton');
+      const loginErrorDiv = $('#divErrorLogin');
+
+      try {
+        pendingButton.addClass("disabled");
+        loginErrorDiv.removeClass('shown');
+        const response = await fetch("/providers/login", {
+          method: "POST",
+          body: JSON.stringify(values)
+        });
+
+        if (!response.ok) {
+          // This means there's something wrong with the api.
+          // TODO Show some error.
+        }
+
+        const responseJson = await response.json();
+
+        if (responseJson.message === "Login successful") {
+          // Redirect to the entryPortal page.
+          window.location.replace("./entryPortal.html");
+        } else {
+          loginErrorDiv.html(responseJson.message);
+          loginErrorDiv.addClass("shown");
+        }
+      } catch (e) {
+        console.log(e);
+      } finally {
+        pendingButton.removeClass("disabled");
       }
-    } catch (e) {
-       console.log("Caught statement");
-    }
-    finally {
-      pendingButton.removeClass("disabled");
     }
   });
 
-  $("#registration-form").on("submit", async (event) => {
+  document.querySelector("#registration-form").addEventListener("submit", async event => {
+  // $("#registration-form").on("submit", async (event) => {
     event.preventDefault();
 
     // We will check that the password fields are the same.
-    const form = document.querySelector("#registration-form");
+    const form = event.target as HTMLFormElement;
     const allQuestions = form.querySelectorAll("form-question") as NodeListOf<FormQuestion>;
     const emailInput = (this.querySelector("#registerEmailInput") as FormQuestion).input;
     const passwordInputOne = (this.querySelector("#registerPasswordInput") as FormQuestion).input;
@@ -79,7 +95,7 @@ $(function() {
       // Verify that the question is valid.
       if (!question.checkValidity()) {
         // Display the error
-        console.log("Found invalid question " + question.id);
+        // console.log("Found invalid question " + question.id);
         question.input.displayError("Invalid input");
         submittable = false;
       }
@@ -87,7 +103,7 @@ $(function() {
 
     if (submittable) {
       if (password === passwordConfirm) {
-        console.log("Passwords match");
+        // console.log("Passwords match");
         // noMatch.removeClass("shown");
         // Make the request.
         const request = {
@@ -114,10 +130,10 @@ $(function() {
         // noMatch.addClass("shown");
         passwordInputOne.displayError("Passwords do not match");
         passwordInputTwo.displayError("Passwords do not match");
-        console.log("Passwords don't match");
+        // console.log("Passwords don't match");
       }
     } else {
-      console.log("Form is not submittable");
+      // console.log("Form is not submittable");
     }
   });
 })
