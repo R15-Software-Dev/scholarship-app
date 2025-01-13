ff28f8c304f204a29c3516121b2c3a411b76a212
meraghon <mdayton0521@gmail.com>
2025-01-13 13:05:05 -0500

    Reverted changes to switchTab.ts, removed 'checked' from first tab

--- src/scripts/switchTab.ts
+++ src/scripts/switchTab.ts
@@ -1,10 +1,8 @@
 import {Tab, TabBar} from "../customElements/Tabs";
 $(function(){
-  const tabBar = document.querySelector("tab-bar") as TabBar;
-  const activeTab = tabBar.activeTab;
-  // Set checked to false initially
-  activeTab.checked = false;
   $("form-section").on("submit-complete", (event) => {
+    const tabBar = document.querySelector("tab-bar") as TabBar;
+    const activeTab = tabBar.activeTab;
     // Adds checkmark to tab upon successful submission
     activeTab.checked = true;
     // Moves to next tab
