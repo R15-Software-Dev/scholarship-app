263b47191a449a2d59ba72775dc75786e7a06332
meraghon <mdayton0521@gmail.com>
2025-01-31 11:19:54 -0500

    passwordReset function framework

---
+++ src/scripts/passwordReset.ts
@@ -0,0 +1,9 @@
+import {ActionButton} from "../customElements/ActionButton";
+import {FormQuestion} from "../customElements/Forms";
+
+$(function(){
+
+  document.querySelector("#resetForm").addEventListener("submit", async event => {
+
+  })
+})
