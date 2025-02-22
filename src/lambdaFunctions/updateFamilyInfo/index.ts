a8f3b9ed136ff0d4c24fdb91ed23d695d7a66455
meraghon <mdayton0521@gmail.com>
2025-02-22 15:53:04 -0500

    Added attributes

--- src/lambdaFunctions/updateFamilyInfo/index.ts
+++ src/lambdaFunctions/updateFamilyInfo/index.ts
@@ -1,5 +1,5 @@
 import { DynamoDBClient, UpdateItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { AWSRequest, AWSResponse, ScholarshipContactInfo } from "./../types/types";
+import { AWSRequest, AWSResponse} from "./../types/types";
 
 const client = new DynamoDBClient({ region: "us-east-1" });
 
@@ -20,7 +20,49 @@ export async function handler(event: AWSRequest): Promise<AWSResponse> {
     Key: {
       //Key
     },
-
+    ExpressionAttributeNames: {
+      "#numChildTotal": "numChildTotal",
+      "#numChildAttendCollege": "numChildAttendCollege",
+      "#guardianOneName": "guardianOneName",
+      "#guardianOneRelation": "guardianOneRelation",
+      "#guardianOneOccupation": "guardianOneOccupation",
+      "#guardianOneEmployer": "guardianOneEmployer",
+      "#guardianTwoName": "guardianTwoName",
+      "#guardianTwoRelation": "guardianTwoRelation",
+      "#guardianTwoOccupation": "guardianTwoOccupation",
+      "#guardianTwoEmployer": "guardianTwoEmployer",
+      "#familyPEAMember": "familyPEAMember",
+      "#armedServiceMember": "armedServiceMember",
+      "#familyChurchMember": "familyChurchMember"
+    },
+    ExpressionAttributeValues: {
+      ":numChildTotal": {S: info.numChildTotal},
+      ":numChildAttendCollege": {S: info.numChildAttendCollege},
+      ":guardianOneName": {S: info.guardianOneName},
+      ":guardianOneRelation": {S: info.guardianOneRelation},
+      ":guardianOneOccupation": {S: info.guardianOneOccupation},
+      ":guardianOneEmployer": {S: info.guardianOneEmployer},
+      ":guardianTwoName": {S: info.guardianTwoName},
+      ":guardianTwoRelation": {S: info.guardianTwoRelation},
+      ":guardianTwoOccupation": {S: info.guardianTwoOccupation},
+      ":guardianTwoEmployer": {S: info.guardianTwoEmployer},
+      ":familyPEAMember": {SS: JSON.parse(info.familyPEAMember)},
+      ":armedServiceMember": {SS: JSON.parse(info.armedServiceMember)},
+      ":familyChurchMember": {SS: JSON.parse(info.familyChurchMember)}
+    },
+    UpdateExpression: "SET #numChildTotal = :numChildTotal," +
+      "#numChildAttendCollege = :numChildAttendCollege," +
+      "#guardianOneName = :guardianOneName," +
+      "#guardianOneRelation = :guardianOneRelation," +
+      "#guardianOneOccupation = :guardianOneOccupation," +
+      "#guardianOneEmployer = :guardianOneEmployer," +
+      "#guardianTwoName = :guardianTwoName," +
+      "#guardianTwoRelation = :guardianTwoRelation," +
+      "#guardianTwoOccupation = :guardianTwoOccupation," +
+      "#guardianTwoEmployer = :guardianTwoEmployer," +
+      "#familyPEAMember = :familyPEAMember," +
+      "#armedServiceMember = :armedServiceMember," +
+      "#familyChurchMember = :familyChurchMember"
   });
 
 
