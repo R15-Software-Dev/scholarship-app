a363ca19b42de7038f838dca2a6cf07959116012
meraghon <mdayton0521@gmail.com>
2025-02-22 13:29:10 -0500

    Create framework for lambda function

---
+++ src/lambdaFunctions/readFamilyInfo/index.ts
@@ -0,0 +1,54 @@
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import { AWSRequest, AWSResponse } from '../types/aws';
+
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+
+/**
+ * Reads family data from the ___ table
+ * @param {string} event - The scholarship name
+ * @returns family information
+ */
+export async function handler(event: AWSRequest): Promise<AWSResponse> {
+  const input: string = JSON.parse(event.body);
+
+  const command = new GetItemCommand({
+    TableName: "",
+    Key: {
+      //Key
+    },
+    AttributesToGet: [
+      "numChildTotal",
+      "numChildAttendCollege",
+      "guardianOneName",
+      "guardianOneRelation",
+      "guardianOneAge",
+      "guardianOneOccupation",
+      "guardianOneEmployer",
+      "guardianTwoName",
+      "guardianTwoRelation",
+      "guardianTwoOccupation",
+      "guardianTwoEmployer",
+      "familyPEAMember",
+      "armedServiceMember",
+      "familyChurchMember"
+    ]
+  });
+
+  // Send the command
+  const dbresponse = await client.send(command);
+
+  // Check that there is an item in the response
+  if (!dbresponse.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: "Item not found" })
+    };
+  }
+
+  // Return the item's information
+  return {
+    statusCode: 200,
+    body: JSON.stringify(dbresponse.Item)
+  };
+}
