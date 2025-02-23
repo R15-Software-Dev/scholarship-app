b085eb4397783fa6b67812b79135730b1e918801
meraghon <mdayton0521@gmail.com>
2025-02-23 15:26:16 -0500

    Moved return statement into try-catch

--- src/lambdaFunctions/updateUniversityDetails/index.ts
+++ src/lambdaFunctions/updateUniversityDetails/index.ts
@@ -70,6 +70,12 @@ export async function handler(event: AWSRequest): Promise<AWSResponse> {
     // Send the command
     const dbresponse = await client.send(command);
 
+    // Return that there was a success
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Success" }),
+    };
+
   } catch (e) {
     console.error(e.message);
     // Return that there was an error
@@ -78,10 +84,4 @@ export async function handler(event: AWSRequest): Promise<AWSResponse> {
       body: JSON.stringify({ message: e.message }),
     }
   }
-
-  // Return that there was a success
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Success" }),
-  };
 }
