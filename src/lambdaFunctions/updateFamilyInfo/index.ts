134cfe9c33149e51f21ada492c115fabc43dfd67
meraghon <mdayton0521@gmail.com>
2025-02-23 15:23:20 -0500

    Fixed comment

--- src/lambdaFunctions/updateFamilyInfo/index.ts
+++ src/lambdaFunctions/updateFamilyInfo/index.ts
@@ -5,7 +5,7 @@ const client = new DynamoDBClient({ region: "us-east-1" });
 
 /**
  * Creates and or updates a record in the student applications table in DynamoDB
- * @param {FamilyInfo}event - A family info object
+ * @param {AWSRequest} event - Request that contains a family info object.
  * @returns DynamoDB response object
  */
 export async function handler(event: AWSRequest): Promise<AWSResponse> {
