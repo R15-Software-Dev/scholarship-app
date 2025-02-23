331cb4f17b4971d99a15d98c2636d77cd08db0a6
meraghon <mdayton0521@gmail.com>
2025-02-23 17:11:45 -0500

    Created extracurricular activities object

--- src/lambdaFunctions/types/student.ts
+++ src/lambdaFunctions/types/student.ts
@@ -38,6 +38,10 @@ export type FamilyInfo = {
   familyChurchMember: string;
 }
 
+export type ExtracurricularActivities = {
+
+}
+
 export type InstitutionalResources = {
   grantsAwarded?: string;
   totalSelfHelp?: string;
