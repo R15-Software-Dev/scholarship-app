a2659f2edb5b594c7d0f2d1e31cf108dc33c03e7
meraghon <mdayton0521@gmail.com>
2025-02-27 13:21:22 -0500

    Created a student essays object

--- src/lambdaFunctions/types/student.ts
+++ src/lambdaFunctions/types/student.ts
@@ -85,3 +85,7 @@ export type InstitutionalResources = {
   totalSelfHelp?: string;
   loansValue?: string;
 }
+
+export type StudentEssays = {
+
+}
--- src/lambdaFunctions/types/types.ts
+++ src/lambdaFunctions/types/types.ts
@@ -2,5 +2,5 @@
 
 export { AWSRequest, AWSResponse, AWSAuthRequest, AWSAuthResponse } from "./aws";
 export { ScholarshipContactInfo, ScholarshipInfo, ScholarshipEligibility, ScholarshipRequirements, Scholarship } from "./scholarship";
-export { StudentPersonalInfo, AcademicHonors, AthleticParticipation, CommunityInvolvement, WorkExperience, ExtracurricularActivities, UniversityDetails, FamilyInfo, InstitutionalResources } from "./student";
+export { StudentPersonalInfo, AcademicHonors, AthleticParticipation, CommunityInvolvement, WorkExperience, ExtracurricularActivities, UniversityDetails, FamilyInfo, InstitutionalResources, StudentEssays } from "./student";
 
