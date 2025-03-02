e21dc6391929c29e9f0722460fcb4167ebb3a668
Lucas Vas <lvas463@gmail.com>
2025-03-02 11:09:14 -0500

    feat: create full form initialization script

--- src/scripts/studentEntry.ts
+++ src/scripts/studentEntry.ts
@@ -2,31 +2,38 @@ import {Student} from "../lambdaFunctions/types/types";
 import {InputElement} from "../customElements/InputElement";
 
 document.addEventListener('DOMContentLoaded', async () => {
-  // Log in user.
-  const authCode = new URLSearchParams(window.location.search).get("code");
-  await fetch("/students/login", {
-    method: "POST",
-    body: JSON.stringify({
-      code: authCode
-    })
-  });
-
-  // Initialize form.
-  await fetch("/students/info/all", {
-    method: "GET"
-  })
-    .then(async res => await res.json() as Student)
-    .then(json => {
-      Object.entries(json).forEach(([key, value]) => {
-        console.log(key, value);
-        if (value) {
-          const element = document.querySelector(`[name="${key}"]`) as InputElement;
-          if (element) {
-            console.log("found element", element);
-            element.value = value;
-          }
-        }
+  const loader = $(".loader");
+  loader.show();
+  try {
+    // Log in user.
+    const authCode = new URLSearchParams(window.location.search).get("code");
+    await fetch("/students/login", {
+      method: "POST",
+      body: JSON.stringify({
+        code: authCode
       })
+    });
+
+    // Initialize form.
+    await fetch("/students/info/all", {
+      method: "GET"
     })
-    .catch(err => console.log(err));
+      .then(async res => await res.json() as Student)
+      .then(json => {
+        Object.entries(json).forEach(([key, value]) => {
+          if (value) {
+            const element = document.querySelector(`[name="${key}"]`) as InputElement;
+            if (element) {
+              element.value = value;
+            }
+          }
+        })
+      })
+      .catch(err => console.log(err));
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("hiding loader");
+    loader.fadeOut("fast");
+  }
 });
